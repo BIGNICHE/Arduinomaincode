6f71b347adf467610b9ec2dcccc5d7ac013f4735
Nichesrepository <jenkinsnickb@gmail.com>
2019-05-16 18:34:19 +1000

    no message

--- Arduino_Main_Code.ino
+++ Arduino_Main_Code.ino
@@ -17,10 +17,15 @@ int normalTravelVelocity = 700; //This is in millimeters per second
 
 int stepsLowerScoop = 9300; //Positive Integer that represents the displacement of the scoop from the start position to being low enough to pick up the balls
 int travelTime;
+int switchState;
+
+bool bumpForwardState = false;
+bool bumpBackwardState = false;
 
 Servo backServo;
 Servo leftServo;
 Servo rightServo;
+Servo scoopArticulateServo;
 Stepper elevatorStepper(stepsPerRevolution, 31,32,33,34);
 
 int bumpSwitchForwardPin = 2;
@@ -35,129 +40,128 @@ int currTime;
 
 
 void setup() {
-//  pinMode(4, OUTPUT);
-//  pinMode(5, OUTPUT);
-//  pinMode(6, OUTPUT);
-//  pinMode(7, OUTPUT);
-//  pinMode(8, OUTPUT);
-//  pinMode(9, OUTPUT);
-//  pinMode(10, OUTPUT);
-//  pinMode(11, OUTPUT);
-//  pinMode(12, OUTPUT);
-  pinMode(30, OUTPUT);
-  digitalWrite(30, HIGH);
   pinMode(43, OUTPUT);
   digitalWrite(43, HIGH);       // This block is the enables for the motor drivers
   
-  backServo.attach(46);
-  leftServo.attach(44);
-  rightServo.attach(45);
-  elevatorStepper.setSpeed(150);
+  backServo.attach(28);
+  leftServo.attach(29);
+  rightServo.attach(30);
+  scoopArticulateServo.attach(31);
+  elevatorStepper.setSpeed(60);
   
   pinMode(bumpSwitchForwardPin, INPUT_PULLUP);
   pinMode(bumpSwitchBackPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(bumpSwitchForwardPin), bumpForward, RISING);
-  attachInterrupt(digitalPinToInterrupt(bumpSwitchBackPin), bumpBackward, RISING);
+  attachInterrupt(digitalPinToInterrupt(bumpSwitchForwardPin), bumpForward, FALLING);
+  attachInterrupt(digitalPinToInterrupt(bumpSwitchBackPin), bumpBackward, FALLING);
+  Serial.begin(9600);
+  bumpForwardState = false;
+  bumpBackwardState = false;
+
+  interrupts();
+
+  bumpForwardState = 0;
+  bumpBackwardState = 0;
   
 }
 
 void loop() {
 
-  //lowerArm(stepsLowerScoop);
 
-  //break();
+  delay(4000);
 
-  left(300);
-  delay(1000);
-  right(300);
-  delay(1000);
 
+  scoopArticulateServo.write(50);
+
+
+  forward(4000);
+
+  left(15000);
+
+
+
+  backward(500);
+
+  rotateClockwise(500);
+
+  
+
+  
+
+
+
+
+
+  
 }
 
 void lowerArm(int finalSteps) {
-
-
-  // This block checks if either bump switch is triggered
-//  for (int currentSteps = 0;currentSteps < finalSteps; currentSteps = currentSteps + 200) {
-//    while(digitalRead(forwardBumpSwitch) = HIGH) {
-//      leftServo.write(0);
-//      rightServo.write(0);
-//      backServo.write(90);
-//    }
-//    while(digitalRead(backBumpSwitch) = HIGH) {
-//      leftServo.write(180);
-//      rightServo.write(180);
-//      backServo.write(90);
-//    }
-//
-//    
-//    
-//    stopChassis();
-//    
-//    myStepper.step(-200);
-
-
-
-//Do it with interrupts instead
 
   elevatorStepper.step(-finalSteps);
   
 }
 
-void left(double distance) {
+void raiseArm(int finalSteps) {
+  elevatorStepper.step(finalSteps);
+}
 
+void rotateClockwise(int distance) {
+  int time_current = millis();
+  do {
   backServo.write(180);
-  leftServo.write(60);
-  rightServo.write(60);
-  delay(distance);
+  leftServo.write(0);
+  rightServo.write(0);
+  } while (millis() < time_current + distance);
+  
+  stopChassis();
+  
+}
+
+
+void left(double distance) {
+  int time_current = millis();
+  do {
+  backServo.write(180);
+  leftServo.write(70);
+  rightServo.write(70);
+  } while (millis() < time_current + distance);
+  
   stopChassis();
   
 }
 
 void right(double distance) {
 
+  int time_current = millis();
+  do {
   backServo.write(0);
-  leftServo.write(110);
-  rightServo.write(110);
-  delay(distance);
+  leftServo.write(100);
+  rightServo.write(100);
+  } while (millis() < time_current + distance);
   stopChassis();
   
 }
 
 void forward(double distance) {
   
-//  startTime = millis(); 
-//  travelTime = distance / normalTravelVelocity;
-//  while (millis() < startTime + travelTime) {
-//    rightServo.write(180);
-//    leftServo.write(180);
-//    backServo.write(90);
-//  }
-
-    rightServo.write(0);
-    leftServo.write(180);
-    backServo.write(90);
-    delay(distance);
-
+  int time_current = millis();
+  do {
+  rightServo.write(0);
+  leftServo.write(180);
+  backServo.write(90);
+  } while (millis() < time_current + distance);
   stopChassis();
+  
   
 }
 
 void backward(double distance) {
 
-//  startTime = millis(); 
-//  travelTime = distance / normalTravelVelocity;
-//  while (millis() < startTime + travelTime) {
-//    rightServo.write(0);
-//    leftServo.write(0);
-//    backServo.write(90);
-//  }
-
-    rightServo.write(180);
-    leftServo.write(0);
-    backServo.write(90);
-    delay(distance);
-    
+  int time_current = millis();
+  do {
+  rightServo.write(180);
+  leftServo.write(0);
+  backServo.write(90);
+  } while (millis() < time_current + distance);
   stopChassis();
   
 }
@@ -170,31 +174,22 @@ void stopChassis() {
 }
 
 void bumpForward() {
-
-  stopChassis();
   
-//  while (digitalRead(bumpSwitchBackPin) = HIGH) {
-//    
-//    leftServo.write(180);
-//    rightServo.write(180);
-//    backServo.write(90);
-//  }
 
-  stopChassis();
-  
+  bumpForwardState = 1;
 }
 
 void bumpBackward() {
 
-  stopChassis();
-  
-//  while (digitalRead(bumpSwitchBackPin) = HIGH) {
-//    
-//    leftServo.write(0);
-//    rightServo.write(180);
-//    backServo.write(90);
-//  }
 
-  stopChassis();
+  
+//  do{
+//    switchState = digitalRead(bumpSwitchBackPin);
+//    leftServo.write(180);
+//    rightServo.write(0);
+//    backServo.write(90);
+//  } while (switchState = 0);
+  bumpBackwardState = 1;
+
   
 }
